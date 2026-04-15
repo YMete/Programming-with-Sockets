3c610f0e288d7f62b309a30cb911e1d6f967cefd
Jacob44 <120714287+YMete@users.noreply.github.com>
2026-04-15 13:09:51 +0200

    Create Programming with Sockets.cpp
    Initial client implementation

---
+++ Programming with Sockets.cpp
@@ -0,0 +1,32 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <winsock2.h>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 4444
+#define BUFFER_SIZE 2048
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Socket error!\n";
+        return 1;
+    }
+
+    struct sockaddr_in serverAddr;
+    int len = sizeof(serverAddr);
+
+    std::string serverIP;
+    std::cout << "Enter the server IP (or 127.0.0.1): ";
+    std::getline(std::cin, serverIP);
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP.c_str());
